63407c5ecc92934251a236bf6e49e5bee51f93f1
Robert <robyop.eyeco@gmail.com>
2021-07-30 08:17:47 +0700

    fix env-config, craco-config with dotenv

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,3 +1,11 @@
+const dotenv = require("dotenv");
+// require("dotenv").config({
+//   path: `.env`,
+// });
+require('dotenv/config')
+
+console.log(`craco.config.js`, process.env.REACT_APP_API_URL);
+
 module.exports = {
   style: {
     postcss: {
--- frontend/src/env-config.js
+++ frontend/src/env-config.js
@@ -1,3 +1,3 @@
 console.log(`process.env.REACT_APP_API_URL`, process.env.REACT_APP_API_URL);
 export const API_URL =
-  process.env.REACT_APP_API_URL || "http://157.230.95.164:5000";
+  process.env.REACT_APP_API_URL || "http://localhost:5000";
